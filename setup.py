fa47f1c4632f5fe38f440730c7835f2fce69618b
Devgum <devgum@foxmail.com>
2019-03-28 16:48:45 +0800

    明确版本限制

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 
 import setuptools
 
+
+REQUIRES_PYTHON = '>=3.6.0'
+
 with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
@@ -10,13 +13,14 @@ setuptools.setup(
     version="1.0.1",
     author="Devgum",
     author_email="devgum@foxmail.com",
+    python_requires=REQUIRES_PYTHON,
     description="Local tester for Leetcode.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/devgum/leetcode_tester",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
